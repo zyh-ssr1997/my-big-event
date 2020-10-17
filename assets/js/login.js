214c5ab2a73664e4dcfc4fbeb033730bbe022b3c
zyh <1143232034@qq.com>
2020-10-17 19:12:27 +0800

    第一次提交大事件

---
+++ assets/js/login.js
@@ -0,0 +1,10 @@
+$(function () {
+    $('#link_reg').on('click', function () {
+        $('.login-box').hide();
+        $('.reg-box').show();
+    })
+    $('#link_login').on('click', function () {
+        $('.login-box').show();
+        $('.reg-box').hide();
+    })
+})
